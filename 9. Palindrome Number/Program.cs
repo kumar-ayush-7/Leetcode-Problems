05de8c870be4400f2dcded0920d8b4f95c3daf42
ayush kumar <kumar.ayush7@outlook.com>
2021-09-19 16:48:31 +0530

    added leetcode 9

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,35 @@
+﻿//https://leetcode.com/problems/palindrome-number/
+using System;
+
+namespace _9._Palindrome_Number
+{
+    class Program
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x >= 0 && x < 10)
+                return true;
+            else if (x < 0)
+                return false;
+            else
+            {
+                int reverse = 0, temp = x;
+                while (temp != 0)
+                {
+                    reverse *= 10;
+                    reverse += temp % 10;
+                    temp = temp / 10;
+                }
+                if (x != reverse)
+                    return false;
+            }
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            int input = 5115;
+            Console.WriteLine(IsPalindrome(input));
+        }
+    }
+}
