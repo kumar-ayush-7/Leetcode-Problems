516a915355a8fa703e226645d4d8bd39bf116dc0
ayush kumar <kumar.ayush7@outlook.com>
2021-09-20 00:51:46 +0530

    modified leetcode 14

--- 20. Valid Parentheses/Program.cs
+++ 20. Valid Parentheses/Program.cs
@@ -9,21 +9,42 @@ namespace _20._Valid_Parentheses
         public static bool IsValid(string s)
         {
             Stack<string> st = new Stack<string>();
-            for(int i = 0; i < s.Length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
                 string item = s[i].ToString();
-                if(item == "(" || item == "{" || item == "[")
+                if (item == "(" || item == "{" || item == "[")
                 {
                     st.Push(item);
                 }
                 if (item == ")" || item == "}" || item == "]")
                 {
-                    if(st.Count > 0)
+                    if (st.Count > 0)
                     {
                         string top = st.Pop();
-                        if ((item == ")" && top != "(") || (item == "]" 
-                            && top != "[") || (item == "}" && top != "{"))
-                            return false;
+                        //if ((item == ")" && top != "(") || (item == "]" 
+                        //    && top != "[") || (item == "}" && top != "{"))
+                        switch (item)
+                        {
+                            case ")":
+                                {
+                                    if (top != "(")
+                                        return false;
+                                    break;
+                                }
+                            case "]":
+                                {
+                                    if (top != "[")
+                                        return false;
+                                    break;
+                                }
+                            case "}":
+                                {
+                                    if (top != "{")
+                                        return false;
+                                    break;
+                                }
+
+                        }
                     }
                     else
                         return false;
@@ -36,7 +57,7 @@ namespace _20._Valid_Parentheses
         }
         static void Main(string[] args)
         {
-            string input = "]";
+            string input = "(}";
             Console.WriteLine(IsValid(input));
         }
     }
