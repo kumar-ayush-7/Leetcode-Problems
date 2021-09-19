8e2ecdc688d9e1efe4ba850e4671fb8f46e31ed9
ayush kumar <kumar.ayush7@outlook.com>
2021-09-19 22:11:26 +0530

    addded leetcode 20

---
+++ 20. Valid Parentheses/Program.cs
@@ -0,0 +1,43 @@
+﻿// https://leetcode.com/problems/valid-parentheses/
+using System;
+using System.Collections.Generic;
+
+namespace _20._Valid_Parentheses
+{
+    class Program
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<string> st = new Stack<string>();
+            for(int i = 0; i < s.Length; i++)
+            {
+                string item = s[i].ToString();
+                if(item == "(" || item == "{" || item == "[")
+                {
+                    st.Push(item);
+                }
+                if (item == ")" || item == "}" || item == "]")
+                {
+                    if(st.Count > 0)
+                    {
+                        string top = st.Pop();
+                        if ((item == ")" && top != "(") || (item == "]" 
+                            && top != "[") || (item == "}" && top != "{"))
+                            return false;
+                    }
+                    else
+                        return false;
+                }
+            }
+            if (st.Count > 0)
+                return false;
+            else
+                return true;
+        }
+        static void Main(string[] args)
+        {
+            string input = "]";
+            Console.WriteLine(IsValid(input));
+        }
+    }
+}
