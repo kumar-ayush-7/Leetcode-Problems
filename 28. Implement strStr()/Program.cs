df49ecab6509bc44779bf99d5f9bacc39ae1fb6a
ayush kumar <kumar.ayush7@outlook.com>
2021-09-23 01:10:05 +0530

    Addded leetcode 28

---
+++ 28. Implement strStr()/Program.cs
@@ -0,0 +1,31 @@
+﻿//https://leetcode.com/problems/implement-strstr/
+using System;
+
+namespace _28._Implement_strStr__
+{
+    class Program
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            int hayLen = haystack.Length, neeLen = needle.Length;
+            if (needle == "")
+                return 0;
+            else if (neeLen > hayLen)
+                return -1;
+            else
+            {
+                for( int i=0; i<=hayLen-neeLen; ++i)
+                {
+                    string tmp = haystack.Substring(i, neeLen);
+                    if (tmp == needle)
+                        return i;
+                }
+                return -1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrStr("a","a"));
+        }
+    }
+}
