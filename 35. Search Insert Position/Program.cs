0607aa827f0493f7b47638d30f48e29eb297b87c
ayush kumar <kumar.ayush7@outlook.com>
2021-09-25 01:50:09 +0530

    modified leetcode 35

--- 35. Search Insert Position/Program.cs
+++ 35. Search Insert Position/Program.cs
@@ -21,10 +21,24 @@ namespace _35._Search_Insert_Position
             }
             return 0;
         }
+        public static int SearchRecursive(int[] nums, int target, int low, int high)
+        {
+            if (low > high)
+                return low;
+            int mid = (low + high) >> 1;
+            if (nums[mid] == target)
+                return mid;
+            if (target < nums[mid])
+                return SearchRecursive(nums, target,low, mid -1);
+            else if (target > nums[mid])
+                return SearchRecursive(nums, target,mid + 1, high);
+            else return low;
+        }
         static void Main(string[] args)
         {
             int[] arr = new int[] { 1,3,5,6 };
             Console.WriteLine(SearchInsert(arr, 7));
+            Console.WriteLine(SearchRecursive(arr, 6,0,arr.Length-1));
         }
     }
 }
