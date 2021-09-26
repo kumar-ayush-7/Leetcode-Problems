6c832d41ecb3c771573279103c63fd4f006f3d22
ayush kumar <kumar.ayush7@outlook.com>
2021-09-26 14:23:02 +0530

    modified leecode 66

--- 66. Plus One/Program.cs
+++ 66. Plus One/Program.cs
@@ -8,28 +8,45 @@ namespace _66._Plus_One
     {
         public static int[] PlusOne(int[] digits)
         {
-            Stack<int> res = new Stack<int>();
-            int carry = 0, item = 0, len = digits.Length;
-            ++digits[len - 1];
-            for (int i = len - 1; i >= 0; i--)
+            int len = digits.Length;
+            for(int i = len -1; i>= 0; --i)
             {
-                item = digits[i] + carry;
-                if (item > 9)
+                if(digits[i] < 9)
                 {
-                    item = item % 10;
-                    carry = 1;
+                    ++digits[i];
+                    return digits;
                 }
-                else
-                    carry = 0;
-                res.Push(item);
+                digits[i] = 0;
             }
-            if (carry > 0)
-                res.Push(carry);
-            return res.ToArray();
+            int[] newArr = new int[len + 1];
+            newArr[0] = 1;
+            return newArr;
         }
+        // using stack
+        //public static int[] PlusOne(int[] digits)
+        //{
+        //    Stack<int> res = new Stack<int>();
+        //    int carry = 0, item = 0, len = digits.Length;
+        //    ++digits[len - 1];
+        //    for (int i = len - 1; i >= 0; i--)
+        //    {
+        //        item = digits[i] + carry;
+        //        if (item > 9)
+        //        {
+        //            item = item % 10;
+        //            carry = 1;
+        //        }
+        //        else
+        //            carry = 0;
+        //        res.Push(item);
+        //    }
+        //    if (carry > 0)
+        //        res.Push(carry);
+        //    return res.ToArray();
+        //}
         static void Main(string[] args)
         {
-            int[] arr = { 4, 3, 2, 1 };
+            int[] arr = { 9,5,9 };
             Console.WriteLine($"{string.Join(", ", PlusOne(arr))}");
         }
     }
