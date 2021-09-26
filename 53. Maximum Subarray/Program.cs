11d53653d113f9c8a1c3260a44c943d26033848a
ayush kumar <kumar.ayush7@outlook.com>
2021-09-26 12:36:13 +0530

    Added leetcode 53

---
+++ 53. Maximum Subarray/Program.cs
@@ -0,0 +1,45 @@
+﻿//https://leetcode.com/problems/maximum-subarray/
+using System;
+
+namespace _53._Maximum_Subarray
+{
+    class Program
+    {
+        //O(n^2) approach
+        //public static int MaxSubArray(int[] nums)
+        //{
+        //    int result = int.MinValue; 
+        //    for(int i = 0; i< nums.Length; i++)
+        //    {
+        //        int tempSum = 0;
+        //        for(int j= i; j < nums.Length; j++)
+        //        {
+        //            tempSum += nums[j];
+        //            if (tempSum > result)
+        //                result = tempSum;
+        //        }
+        //    }
+        //    return result;
+        //}
+
+        //O(n) approach
+        public static int MaxSubArray(int[] nums)
+        {
+            int result = int.MinValue, tempSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                tempSum += nums[i];
+                if (tempSum < nums[i])
+                    tempSum = nums[i];
+                if (tempSum > result)
+                    result = tempSum;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 4, -1, 7, 8 };
+            Console.WriteLine(MaxSubArray(nums));
+        }
+    }
+}
