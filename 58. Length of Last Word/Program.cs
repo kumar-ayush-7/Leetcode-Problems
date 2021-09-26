fe03db13eb75caea5ed59a51d29f98cd7f86e0e1
ayush kumar <kumar.ayush7@outlook.com>
2021-09-26 13:34:22 +0530

    added leetcode 58

---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,26 @@
+﻿//https://leetcode.com/problems/length-of-last-word/
+using System;
+
+namespace _58._Length_of_Last_Word
+{
+    class Program
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            int res = 0;
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                if (res > 0 && s[i] == ' ')
+                    return res;
+                if (s[i] != ' ')
+                    ++res;
+
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("luffy is the man who will become the pirate king"));
+        }
+    }
+}
