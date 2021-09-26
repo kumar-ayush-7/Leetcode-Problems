326141f24fba39efc867b7d931e6bc8480291bf9
ayush kumar <kumar.ayush7@outlook.com>
2021-09-26 14:04:11 +0530

    Addded leetcode 66

---
+++ 66. Plus One/Program.cs
@@ -0,0 +1,36 @@
+﻿//https://leetcode.com/problems/plus-one/
+using System;
+using System.Collections.Generic;
+
+namespace _66._Plus_One
+{
+    class Program
+    {
+        public static int[] PlusOne(int[] digits)
+        {
+            Stack<int> res = new Stack<int>();
+            int carry = 0, item = 0, len = digits.Length;
+            ++digits[len - 1];
+            for (int i = len - 1; i >= 0; i--)
+            {
+                item = digits[i] + carry;
+                if (item > 9)
+                {
+                    item = item % 10;
+                    carry = 1;
+                }
+                else
+                    carry = 0;
+                res.Push(item);
+            }
+            if (carry > 0)
+                res.Push(carry);
+            return res.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 3, 2, 1 };
+            Console.WriteLine($"{string.Join(", ", PlusOne(arr))}");
+        }
+    }
+}
