c2ad1831b9e8c3c4b827ba34415b9e808a1a0d46
ayush kumar <kumar.ayush7@outlook.com>
2021-09-28 02:12:18 +0530

    modified leetcode 67

--- 67. Add Binary/Program.cs
+++ 67. Add Binary/Program.cs
@@ -8,20 +8,38 @@ namespace _67._Add_Binary
     {
         public static string AddBinary(string a, string b)
         {
+            //StringBuilder sb = new StringBuilder();
+            //int i = a.Length - 1, j = b.Length - 1, carry = 0;
+            //while (i >= 0 || j >= 0)
+            //{
+            //    int sum = carry;
+            //    if (j >= 0) sum += b[j--] - '0';
+            //    if (i >= 0) sum += a[i--] - '0';
+            //    sb.Append(sum % 2);
+            //    carry = sum / 2;
+            //}
+            //if (carry != 0) sb.Append(carry);
+            //string result = "";
+            //for (int k = sb.Length - 1; k >= 0; k--)
+            //    result += sb[k].ToString();
+            //return result;
             StringBuilder sb = new StringBuilder();
             int i = a.Length - 1, j = b.Length - 1, carry = 0;
-            while (i >= 0 || j >= 0)
+            while(i >= 0 || j>= 0)
             {
                 int sum = carry;
-                if (j >= 0) sum += b[j--] - '0';
-                if (i >= 0) sum += a[i--] - '0';
+                if (i >= 0)
+                    sum += a[i--] - '0';
+                if (j >= 0)
+                    sum += b[j--] - '0';
                 sb.Append(sum % 2);
                 carry = sum / 2;
             }
-            if (carry != 0) sb.Append(carry);
+            if (carry != 0)
+                sb.Append(carry);
             string result = "";
             for (int k = sb.Length - 1; k >= 0; k--)
-                result += sb[k].ToString();
+                result += sb[k];
             return result;
         }
         //public static string AddBinary(string a, string b)
