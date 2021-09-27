ec1af10b61bcbc344072514874d99b975fccd489
ayush kumar <kumar.ayush7@outlook.com>
2021-09-28 03:17:26 +0530

    Added leetcode 69

---
+++ 69. Sqrt(x)/Program.cs
@@ -0,0 +1,33 @@
+﻿//https://leetcode.com/problems/sqrtx/
+using System;
+
+namespace _69._Sqrt_x_
+{
+    class Program
+    {
+        public static int MySqrt(int x)
+        {
+            if (x < 2)
+                return x;
+            else
+            {
+                int low = 1, high = x;
+                while(low < high)
+                {
+                    int mid = low + (high - low)/2;
+                    if (mid == x/mid)
+                        return mid;
+                    else if (mid > x/mid)
+                        high = mid;
+                    else
+                        low = mid + 1;
+                }
+                return low - 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MySqrt(2147395598));
+        }
+    }
+}
