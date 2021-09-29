d52a88e59a217c55470b6be55dd68f628f01668d
ayush kumar <kumar.ayush7@outlook.com>
2021-09-29 20:52:36 +0530

    Added leetcode 70

---
+++ 70. Climbing Stairs/Program.cs
@@ -0,0 +1,61 @@
+﻿//https://leetcode.com/problems/climbing-stairs/
+using System;
+
+namespace _70._Climbing_Stairs
+{
+    class Program
+    {
+        // creating a dp array which will only be till no of steps <= 10 
+        static int[] dp = new int[50];
+        public static int ClimbStairs(int n)
+        {
+
+            int count = 0;
+            count += ClimbStair(n);
+            return count;
+        }
+
+        // using memorization 
+        public static int ClimbStair(int n)
+        {
+            if (n >= 0)
+            {
+                if (dp[n] > 0)
+                    return dp[n];
+                if (n == 0)
+                    return 1;
+                else
+                {
+                    int count = 0;
+                    count += ClimbStair(n - 1);
+                    count += ClimbStair(n - 2);
+                    return dp[n] = count;
+                }
+            }
+            else
+                return 0;
+        }
+        // Simple recursive method
+        //public static int ClimbStair(int n)
+        //{
+        //    if (n >= 0)
+        //    {
+        //        if (n == 0)
+        //            return 1;
+        //        else
+        //        {
+        //            int count = 0;
+        //            count += ClimbStair(n - 1);
+        //            count += ClimbStair(n - 2);
+        //            return count;
+        //        }
+        //    }
+        //    else
+        //        return 0;
+        //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClimbStairs(46));
+        }
+    }
+}
