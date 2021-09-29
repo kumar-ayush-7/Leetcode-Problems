69a86454b09cac73fafabddb862111668057e497
ayush kumar <kumar.ayush7@outlook.com>
2021-09-29 21:05:01 +0530

    modified leetcode 70

--- 70. Climbing Stairs/Program.cs
+++ 70. Climbing Stairs/Program.cs
@@ -5,36 +5,54 @@ namespace _70._Climbing_Stairs
 {
     class Program
     {
-        // creating a dp array which will only be till no of steps <= 10 
-        static int[] dp = new int[50];
+        // Since we know the for any step the maximum number of ways to reach it is the sum of previous two steps
+        // we can create a simple solution
         public static int ClimbStairs(int n)
         {
-
-            int count = 0;
-            count += ClimbStair(n);
-            return count;
+            if (n < 2)
+                return n;
+            int first = 1, Second = 2, itr = 3;
+            while(itr <= n)
+            {
+                first += Second;
+                Second = first - Second;
+                first -= Second;
+                Second += first;
+                ++itr;
+            }
+            return Second;
         }
+
+        // creating a dp array which will only be till no of steps <= 10 
+        //static int[] dp = new int[50];
+        //public static int ClimbStairs(int n)
+        //{
+
+        //    int count = 0;
+        //    count += ClimbStair(n);
+        //    return count;
+        //}
 
         // using memorization 
-        public static int ClimbStair(int n)
-        {
-            if (n >= 0)
-            {
-                if (dp[n] > 0)
-                    return dp[n];
-                if (n == 0)
-                    return 1;
-                else
-                {
-                    int count = 0;
-                    count += ClimbStair(n - 1);
-                    count += ClimbStair(n - 2);
-                    return dp[n] = count;
-                }
-            }
-            else
-                return 0;
-        }
+        //public static int ClimbStair(int n)
+        //{
+        //    if (n >= 0)
+        //    {
+        //        if (dp[n] > 0)
+        //            return dp[n];
+        //        if (n == 0)
+        //            return 1;
+        //        else
+        //        {
+        //            int count = 0;
+        //            count += ClimbStair(n - 1);
+        //            count += ClimbStair(n - 2);
+        //            return dp[n] = count;
+        //        }
+        //    }
+        //    else
+        //        return 0;
+        //}
         // Simple recursive method
         //public static int ClimbStair(int n)
         //{
@@ -55,7 +73,7 @@ namespace _70._Climbing_Stairs
         //}
         static void Main(string[] args)
         {
-            Console.WriteLine(ClimbStairs(46));
+            Console.WriteLine(ClimbStairs(5));
         }
     }
 }
