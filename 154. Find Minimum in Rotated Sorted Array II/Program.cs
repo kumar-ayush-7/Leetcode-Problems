219f21b3ba86f8f05688de3ebbb24e7275b6740d
ayush kumar <kumar.ayush7@outlook.com>
2021-10-09 20:44:45 +0530

    Added leetcode 154

---
+++ 154. Find Minimum in Rotated Sorted Array II/Program.cs
@@ -0,0 +1,33 @@
+﻿//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+using System;
+
+namespace _154._Find_Minimum_in_Rotated_Sorted_Array_II
+{
+    class Program
+    {
+        public static int FindMin(int[] nums)
+        {
+            int high = nums.Length - 1, low = 0;
+            while (high > low)
+            {
+                int mid = low + (high - low) / 2;
+                if (nums[mid] == nums[high])
+                    high = high - 1;
+                else
+                {
+
+                    if (nums[mid] < nums[high])
+                        high = mid;
+                    else
+                        low = mid + 1;
+                }
+            }
+            return nums[low];
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2, 2, 0, 1 };
+            Console.WriteLine(FindMin(nums));
+        }
+    }
+}
