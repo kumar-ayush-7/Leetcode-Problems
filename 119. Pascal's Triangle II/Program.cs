0eb84e10228c9af2f9daa0b3abcc8ac7baf24d36
ayush kumar <kumar.ayush7@outlook.com>
2021-10-11 23:39:04 +0530

    Added leetcode 119

---
+++ 119. Pascal's Triangle II/Program.cs
@@ -0,0 +1,34 @@
+﻿//https://leetcode.com/problems/pascals-triangle-ii/
+using System;
+using System.Collections.Generic;
+
+namespace _119._Pascal_s_Triangle_II
+{
+    class Program
+    {
+        public static IList<int> GetRow(int rowIndex)
+        {
+            IList<IList<int>> pas = new List<IList<int>>();
+            IList<int> first = new List<int>();
+            first.Add(1);
+            pas.Add(first);
+            for (int i = 1; i < rowIndex + 1; i++)
+            {
+                IList<int> item = new List<int>();
+                IList<int> prev = pas[i - 1];
+                item.Add(1);
+                for (int j = 1; j < i; j++)
+                {
+                    item.Add(prev[j - 1] + prev[j]);
+                }
+                item.Add(1);
+                pas.Add(item);
+            }
+            return pas[rowIndex];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
